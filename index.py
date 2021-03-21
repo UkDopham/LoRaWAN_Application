2e3f58baf5a3a9960550e9f12fbb9cca27d38616
A.Dopham <alexandre34980@outlook.fr>
2021-03-21 16:27:52 +0100

    Update index.py

--- index.py
+++ index.py
@@ -42,7 +42,7 @@ if (isPassCorrect):
             print("Data : " + str(tempBytes))
             #temperature = (  ((((int(tempBytes[0])<<8) + (int(tempBytes[1]))/65536.0)*165.0 ) - 40.0  ) )
             temperature = ((int(tempBytes[0]) <<8) + int(tempBytes[1]))/100
-            print("Device " + str(tempBytes[2]) + " " + str(temperature))
+            print("Device " + str(int(tempBytes[2])) + " " + str(temperature))
             #print(byte_xor(s, APPKEY))
         #decrypt=byte_xor(ser.readline(), APPKEY)
         #if decrypt != b'':
